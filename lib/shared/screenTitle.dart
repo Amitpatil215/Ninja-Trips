71132f1006fb2916938ea8c9fe140b58983d2210
Amit Patil <amitgpatil215@gmail.com>
2020-12-24 19:55:27 +0530

    tween animation builder for title

--- lib/shared/screenTitle.dart
+++ lib/shared/screenTitle.dart
@@ -7,9 +7,28 @@ class ScreenTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+    return TweenAnimationBuilder(
+      tween: Tween<double>(begin: 0, end: 1),
+      duration: Duration(seconds: 2),
+      builder: (context, value, child) {
+        return Opacity(
+          opacity: value, // It takes value as a double, provided in tween.
+
+          /// Workaround 1:
+          // child: child, // It automatically looks for child of builder and wrpas it with opacity
+
+          /// Workaround 2:
+          // We wanna animate the paddind along with opacity
+          child: Padding(
+            padding: EdgeInsets.only(top: value * 20),
+            child: child, // It automatically looks for child of builder and wrpas it with opacity and padding
+          ),
+        );
+      },
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
